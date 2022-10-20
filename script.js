9ba374bd023c69c6c5c29b3ae95f9df203a22778
Aliakbar <nugmanov.aliakbar@gmail.com>
2022-10-20 23:57:55 +0500

    jshomework

---
+++ script.js
@@ -0,0 +1,35 @@
+var username = prompt('Ismingizni kiriting: ')
+
+alert('Assalomu aleykum ' + username)
+console.info('Sizning ismingiz: ' + username)
+
+var userage = +prompt('Yoshingizni kiriting: ')
+
+console.info('Sizning yoshingiz: ' + userage)
+
+var addition = +prompt('Iltimos javobini yozing: 6 + 3 = ')
+
+// alert('savolga javobingiz: ' + addition)
+console.info('6 + 3 = ' + (6 + 3) + ' (sizning javobingiz: ' + addition + ')')
+
+var diff = +prompt('Iltimos javobini yozing: 6 - 3 = ')
+
+// alert('savolga javobingiz: ' + diff)
+console.info('6 - 3 = ' + (6 - 3) + ' (sizning javobingiz: ' + diff + ')')
+
+var mult = +prompt('Iltimos javobini yozing: 6 * 3 = ')
+
+// alert('savolga javobingiz: ' + mult)
+console.info('6 * 3 = ' + (6 * 3) + ' (sizning javobingiz: ' + mult + ')')
+
+var div = +prompt('Iltimos javobini yozing: 6 / 3 = ')
+
+// alert('savolga javobingiz: ' + div)
+console.info('6 / 3 = ' + (6 / 3) + ' (sizning javobingiz: ' + div + ')')
+
+var reminder = +prompt('Iltimos javobini yozing: 7 % 3 = ')
+
+// alert('savolga javobingiz: ' + reminder)
+console.info('7 % 3 = ' + (7 % 3) + ' (sizning javobingiz: ' + reminder + ')')
+
+
